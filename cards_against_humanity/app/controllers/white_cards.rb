d6940eeed4abba64f61d2dd0a309ff60978463d6
immber <jessiekrushing@hotmail.com>
2015-02-04 22:04:07 -0800

    crud is happening

--- cards_against_humanity/app/controllers/white_cards.rb
+++ cards_against_humanity/app/controllers/white_cards.rb
@@ -1,6 +1,7 @@
 
 get '/white_cards' do
-  @white_cards = [1,2,3] #Cards.where(type: 'white', created_by: "user_id")
+  user_id = 1
+  @white_cards = Card.where(color: "white", created_by: 1)
   erb :"/white_cards/show_white_cards"
 end
 
@@ -9,16 +10,24 @@ get '/white_cards/new' do
 end
 
 post '/white_cards' do
+  Card.create(color: "white", text: params[:card_text], created_by: 1)
+  redirect '/white_cards'
 end
 
-get '/white_cards/:id' do
+put '/white_cards/:id' do
+  update_card = Card.find(params[:id])
+  update_card.update_attributes(text: params[:card_text])
+  redirect '/white_cards'
+end
+
+delete '/white_cards/:id' do
+  Card.destroy(params[:id])
+  redirect '/white_cards'
 end
 
 get '/white_cards/:id/edit' do
+  @card = Card.find(params[:id])
+  erb :"/white_cards/edit_white_card"
 end
 
-put 'white_cards/:id' do
-end
 
-delete 'white_cards/:id' do
-end
